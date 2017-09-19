3f06b3122217a01a66329570ecb428d71f33dcc4
8isoto <31480943+8isoto@users.noreply.github.com>
2017-09-19 15:33:52 -0500

    Add files via upload

--- loops.js
+++ loops.js
@@ -1,51 +1,50 @@
-function loopOne(time){
+function loopOne(Time){
+let time = 30;
+while(time > 0){
+  if(time %5 == 0){
 
-  while(time >= 0){
-    if(time %5 == 0){
-      console.log("AHHHHHH"+time+"seconds left");
-    }
-    else{
-      console.log(time+"seconds left.");
-    }
-    time=time-1;
-  }
-  console.log("Done Son!")
+  console.log("AHHHHHH" + time + "seconds left!");
 }
+else{
+  console.log(time + "seconds left.");
+}
+  time = time - 1;
+}
+console.log("Done Son!");
+}
+
 function loopTwo(number,power){
-  let sum =0;
-  for(let p=0; p <=power; p++){
-    sum = sum + Math+pow(number, p);
-  }
-   return sum;
+let sum=0;
+for(let p= 0; p<=power; p++){
+sum = sum + Math.pow(number,p);
+}
+return sum
+}
 
- }
+function loopThree(symbol,number){
 
-
-function loopThree(number,number){
-  let lastime="";
-  for (let p = 1;p<=number;p++){
-    lastime=lastime+pyramid
-    console.log(lastime);
-
-  }
-
+  let lasttime="";
+for(let s=0; s<=number; s++){
+lasttime=lasttime+symbol;
+console.log(lasttime);
+}
 }
 
 function main(){
-  //Testing each function
-  loopOne(10);
-  loopOne(25);
+    //Testing each function
+    loopOne(10);
+    loopOne(25);
 
-  let powersOfTwo = loopTwo(2,10);
-  let powersOfFive = loopTwo(5,4);
-  console.log("The sum is "+powersOfTwo);
-  console.log("The sum is "+powersOfFive);
+    let powersOfTwo = loopTwo(2,10);
+    let powersOfFive = loopTwo(5,4);
+    console.log("The sum is "+powersOfTwo);
+    console.log("The sum is "+powersOfFive);
 
-  loopThree("#",6);
-  loopThree("$",20);
-  loopThree("x",10);
+    loopThree("#",6);
+    loopThree("$",20);
+    loopThree("x",10);
 
-  console.log("You will need to scroll up to check all of them.");
+    console.log("You will need to scroll up to check all of them.");
 }
 
 //Hit ctrl + Shift + B to run the program.
